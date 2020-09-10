f85ae9af24a80f49a8979218eb399052a1bd854f
Priyanga Kulamithra <priyanga.kulamithra@gmail.com>
2020-09-10 14:14:19 +0200

    Add project files.

---
+++ Inlämningsuppgift 2, Butiksdatasystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inlämningsuppgift_2__Butiksdatasystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
