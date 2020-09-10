16288a530396ea1166d5c91ee70ee8ed4466cf98
Priyanga Kulamithra <priyanga.kulamithra@gmail.com>
2020-09-10 14:36:59 +0200

    Created method for printing KASSA-menu

--- Inlämningsuppgift 2, Butiksdatasystem/Program.cs
+++ Inlämningsuppgift 2, Butiksdatasystem/Program.cs
@@ -4,9 +4,15 @@ namespace Inlämningsuppgift_2__Butiksdatasystem
 {
     class Program
     {
+        static void PrintKassaMenu()
+        {
+            Console.WriteLine("KASSA");
+            Console.WriteLine("1. Ny kund");
+            Console.WriteLine("0. Avsluta");
+        }
         static void Main(string[] args)
         {
-            
+            PrintKassaMenu();
         }
     }
 }
